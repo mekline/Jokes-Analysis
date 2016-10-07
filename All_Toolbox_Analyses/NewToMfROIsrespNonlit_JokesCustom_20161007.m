873a087c9614d697365f92caf57a827e1ae39e12
mekline@mit.edu <melissa.e.kline@gmail.com>
2016-10-07 16:14:16 -0400

    same with a real output dir!!

--- All_Toolbox_Analyses/NewToMfROIsrespNonlit_JokesCustom_20161007.m
+++ All_Toolbox_Analyses/NewToMfROIsrespNonlit_JokesCustom_20161007.m
@@ -33,7 +33,7 @@ MyDataFolder = '/users/evelina9/more_evelina_files/MEGADATA.now.on.evlabfs'; % p
 
 participant_sessions = {{'152_KAN_RHnonlit_01','138_KAN_RHnonlit_02','136_KAN_RHnonlit_03','194_KAN_RHnonlit_04','097_KAN_RHnonlit_05','135_KAN_RHnonlit_06','197_KAN_RHnonlit_07','198_KAN_RHnonlit_08','018_KAN_RHnonlit_09','049_KAN_RHnonlit_10','201_KAN_RHnonlit_11','175_KAN_RHnonlit_12'}}; %The subject IDs of individual subjects you'll analyze
 
-MyOutputFolder = '/mindhive/evlab/u/mekline/Desktop/Jokes/New_Analyses/NewToMfROIsrespNonlitJokesCustom_20160203_results'; %Where should the results wind up? For testing this script, this is all u need to change and it should just work! Note: usually the scripts are good about this but this one actually does break if you put a slash at the end of your dir name.
+MyOutputFolder = '/mindhive/evlab/u/mekline/Desktop/Jokes/New_Analyses/NewToMfROIsrespNonlitJokesCustom_20161007_results'; %Where should the results wind up? For testing this script, this is all u need to change and it should just work! Note: usually the scripts are good about this but this one actually does break if you put a slash at the end of your dir name.
 
 firstlevel_loc = 'firstlevel_ToMshort'; % path to the first-level analysis directory for the lang localizer or whatever
 firstlevel_crit = 'firstlevel_nonlit_joke_custom'; %path to 1st-level analysis directory for the critical task
